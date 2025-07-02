361f7f5f4b55cdbd8193f31a4a5a30f3b8902478
Brian Ombura <brianombura@Brians-MacBook-Pro.local>
2025-07-02 17:39:15 -0500

    renamed project name to realtime-poll

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the recipe backend." });
+  res.json({ message: "Welcome to the realtime-poll backend." });
 });
 
 require("./app/routes/auth.routes.js")(app);
@@ -35,7 +35,7 @@ require("./app/routes/recipeIngredient.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3200;
+const PORT = process.env.PORT || 3201;
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
