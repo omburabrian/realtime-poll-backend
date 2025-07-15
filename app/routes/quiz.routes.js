9f1a4013179920913a53e40ec2d079fb9be080a8
thierrynsanzabera <thierry.nsanzabera@eagles.oc.edu>
2025-07-14 22:43:33 -0500

    backend functionalities for QR codes

---
+++ app/routes/quiz.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const QRCode = require('qrcode');
+const router = express.Router();
+
+
+router.get('/quiz/:id/qrcode', async (req, res) => {
+  const quizId = req.params.id;
+  //const quizLink = `https://website.com/quiz/${quizId}`;
+
+  try {
+    const qrCodeDataURL = await QRCode.toDataURL(quizLink);
+    res.status(200).json({ qrCode: qrCodeDataURL });
+  } catch (error) {
+    console.error('QR Code Generation Error:', error);
+    res.status(500).json({ error: 'Failed to generate QR Code' });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -33,7 +33,10 @@ require("./app/routes/recipe.routes")(app);
 require("./app/routes/recipeStep.routes")(app);
 require("./app/routes/recipeIngredient.routes")(app);
 require("./app/routes/user.routes")(app);
+require("./app/routes/quiz.routes")(app);
+const quizRoutes = require('./routes/quizRoutes');
 
+app.use('/api', quizRoutes);
 // set port, listen for requests
 const PORT = process.env.PORT || 3201;
 if (process.env.NODE_ENV !== "test") {
