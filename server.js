929a071bbf65c4496e8825ec4759b390879bfd1b
Glen Davis <glen.davis@oc.edu>
2025-07-28 04:35:38 -0500

    Checks for default ADMIN user and adds it, if does not exist

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const app = express();
 
 const db = require("./app/models");
 
-db.sequelize.sync();
+//  Get data needed for creating default admin user, if needed.
+const { USER_ROLES } = require("./app/config/constants");
+const { getSalt, hashPassword } = require("./app/authentication/crypto");
+
+//  Sync all defined models with the database.
+db.sequelize.sync()
+  .then(() => {
+    //  If models and DB successfully synced, perform any required DB initialization.
+    console.log("Synced DB.");
+    initializeDatabase();
+  });
 
 var corsOptions = {
   origin: "http://localhost:8081",
@@ -26,6 +36,58 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the realtime-poll backend." });
 });
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+async function initializeDatabase() {
+  try {
+    await ensureAdminUserExists();
+  } catch (error) {
+    console.error("Error initializing admin user: ", error);
+  }
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+async function ensureAdminUserExists() {
+  try {
+
+    //  Check whether the admin USER already exists, and if not, create it.
+    const adminUser = await db.user.findOne({ where: { userName: "admin" } });
+
+    if (!adminUser) {
+      console.log("Creating default admin user...");
+
+      const temporaryDefaultAdminPassword = "password123";
+      const temporaryDefaultAdminEmail = "admin@rtpoll.com";
+
+      //  Get admin password fron .env file.
+      const adminPassword = process.env.DEFAULT_ADMIN_PASSWORD || temporaryDefaultAdminPassword;
+      if (adminPassword === temporaryDefaultAdminPassword) {
+        console.warn("WARNING: Using a default admin password.  Please set DEFAULT_ADMIN_PASSWORD in your .env file.");
+      }
+
+      //  Encrypt the credentials.
+      const salt = await getSalt();
+      const hash = await hashPassword(adminPassword, salt);
+
+      //  Create the admin user, with the ADMIN ROLE.
+      await db.user.create({
+        firstName: "Admin",
+        lastName: "User",
+        userName: "admin",
+        email: process.env.DEFAULT_ADMIN_EMAIL || temporaryDefaultAdminEmail,
+        role: USER_ROLES.ADMIN,
+        password: hash,
+        salt: salt,
+      });
+      console.log("Default admin user created successfully.");
+    }
+  } catch (error) {
+    console.error("Error during initial admin user creation: ", error);
+  }
+}
+
+//-------------------------------------------------------------------------
+//  INITIALIZE API ROUTES
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //  Users, Authentication, and Sessions
