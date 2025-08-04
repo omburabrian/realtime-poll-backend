3f001863ca53c03225231dade9b7fd6f9367078b
Glen Davis <glen.davis@oc.edu>
2025-08-04 16:57:21 -0500

    Adds test data for COURSES

--- app/models/course.model.js
+++ app/models/course.model.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
     const Course = sequelize.define("course", {
-        name: {
+        title: {
             type: Sequelize.STRING,
             allowNull: false,
         },
